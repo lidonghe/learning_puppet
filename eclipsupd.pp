e70a9ea3ecb65fc634bc996907c25b207695d0a4
lidong <lidong@debian>
2014-12-26 08:22:58 -0500

    change name by site

---
+++ eclipsupd.pp
@@ -0,0 +1,54 @@
+class {'apache':
+    default_vhost       => false,
+    default_mods        => false,
+    default_confd_files => false,
+    mpm_module          => 'prefork', 
+}
+
+include 'apache::mod::wsgi'
+include 'apache::mod::php'
+
+apache::vhost{'eclipsupd':
+    port => '80',
+    docroot => '/var/www',
+    wsgi_script_aliases => {
+        '/wsgi' => '/var/www/wsgi/code.py' ,
+        '/svnlog' => '/var/www/svnlog/ldap_handler.py',
+        '/ILItfChg' => '/var/www/ILItfChg/wsgi_handler.py',
+    },
+    aliases => [
+        {
+            alias => '/wsgi/static', 
+            path => '/var/www/wsgi/static/',
+        },
+        {
+            alias => '/svnlog/static',
+            path => '/var/www/svnlog/static',
+        },
+        {
+            alias => '/static', 
+            path => '/var/www/ILItfChg/static',
+        },
+    ],
+}
+
+python::pip{'django':
+    pkgname => 'django',
+    ensure => '1.3.7',
+    proxy => 'http://10.144.1.10:8080',
+}
+package{'python-ldap':
+    ensure => present,
+}
+package{'python-mysqldb':
+    ensure => present,
+}
+file{'/var/www/svnlog':
+    ensure => 'link',
+    target => '/home/lidong/dist/svnlog',
+}
+file{'/var/www/ILItfChg':
+    ensure => 'link',
+    target => '/home/lidong/dist/ILItfChg',
+}
+
