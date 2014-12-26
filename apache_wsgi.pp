04be71f3b869e77d68fa085ec8b0e1669a106d03
lidong <lidong@debian>
2014-12-26 06:38:26 -0500

    updated

--- apache_wsgi.pp
+++ apache_wsgi.pp
@@ -7,14 +7,42 @@ class {'apache':
 
 include 'apache::mod::wsgi'
 include 'apache::mod::php'
-apache::vhost{'apache_vhost':
+
+apache::vhost{'eclipsupd':
     port => '80',
     docroot => '/var/www',
-    wsgi_script_aliases => { '/wsgi' => '/var/www/wsgi/code.py' },
+    wsgi_script_aliases => {
+        '/wsgi' => '/var/www/wsgi/code.py' ,
+        '/svnlog' => '/var/www/svnlog/ldap_handler.py',
+        '/ILItfChg' => '/var/www/ILItfChg/wsgi_handler.py',
+    },
     aliases => [
         {
             alias => '/wsgi/static', 
             path => '/var/www/wsgi/static/',
         },
-    ]
+        {
+            alias => '/svnlog/static',
+            path => '/var/www/svnlog/static',
+        },
+        {
+            alias => '/static', 
+            path => '/var/www/ILItfChg/static',
+        },
+    ],
 }
+package{'python-ldap':
+    ensure => present,
+}
+package{'python-mysqldb':
+    ensure => present,
+}
+file{'/var/www/svnlog':
+    ensure => 'link',
+    target => '/home/lidong/dist/svnlog',
+}
+file{'/var/www/ILItfChg':
+    ensure => 'link',
+    target => '/home/lidong/dist/ILItfChg',
+}
+
